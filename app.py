d155f54fc5c426cad64ea9066c47fb336ef80ae0
A-A09 <148891142+A-A09@users.noreply.github.com>
2024-11-18 18:07:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
-
-
-
-
-st.header("Padel Journey")
+st.header("Football Journey")
 
 # Streamlit app
 with st.sidebar: 
@@ -15,7 +11,7 @@ with st.sidebar:
 if choice =="Intro":
     st.text("\n")
     st.subheader("Intro")
-    st.text("This is the title and a short welcome message: Kick-off Basics: The Fundamentals of Padel Welcome to my padel learning journey! Here, you will find a series of insightful instructional videos designed to showcase my progress as I learn the fundamentals o fpadel, including essential skills, rules, and techniques. Whether you're a beginner or just curious about the process, these videos aim to inspire and guide you through the basics of the sport. Have fun!")
+    st.text("This is the title and a short **welcome** message: Kick-off Basics: The Fundamentals of Padel Welcome to my Football learning journey! Here, you will find a series of insightful instructional videos designed to showcase my progress as I learn the fundamentals of football, including essential skills, rules, and techniques. Whether you're a beginner or just curious about the process, these videos aim to inspire and guide you through the basics of the sport. Have fun!")
     
 if choice == "Videos":
 
