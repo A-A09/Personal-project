895d825733cd007b8e30a79248a77a49aed90a68
A-A09 <148891142+A-A09@users.noreply.github.com>
2024-11-18 18:08:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with st.sidebar:
 if choice =="Intro":
     st.text("\n")
     st.subheader("Intro")
-    st.text("This is the title and a short **welcome** message: Kick-off Basics: The Fundamentals of Padel Welcome to my Football learning journey! Here, you will find a series of insightful instructional videos designed to showcase my progress as I learn the fundamentals of football, including essential skills, rules, and techniques. Whether you're a beginner or just curious about the process, these videos aim to inspire and guide you through the basics of the sport. Have fun!")
+    st.text("This is the title and a short welcome message: Kick-off Basics: The Fundamentals of Padel Welcome to my Football learning journey! Here, you will find a series of insightful instructional videos designed to showcase my progress as I learn the fundamentals of football, including essential skills, rules, and techniques. Whether you're a beginner or just curious about the process, these videos aim to inspire and guide you through the basics of the sport. Have fun!")
     
 if choice == "Videos":
 
